8a5c1b3fb5e1c8d2eb94d433e729d3332ffeab8e
ErickS1705 <226w0882@zongolica.tecnm.mx>
2025-06-11 09:08:03 -0600

    Add .next/static/chunks/src_app_curso_[id]_page_tsx_a993527f._.js 1749654483624

---
+++ .next/static/chunks/src_app_curso_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_839f17b6._.js",
+  "static/chunks/src_6df72cea._.js"
+],
+    source: "dynamic"
+});
